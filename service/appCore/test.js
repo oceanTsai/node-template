aa9be96c65234be9490727c05b48a4bc35026c6b
ocean <contest.start@gmai.com>
2016-04-28 14:26:22 +0800

    update test example.

--- service/appCore/test.js
+++ service/appCore/test.js
@@ -1,8 +1,13 @@
-var app = require('express')();
-var swig = require('swig');
-var path = require("path");
-var people;
 console.log('excute test.js');
+const express = require('express');
+const app  = express();
+const swig = require('swig');
+const path = require("path");
+const fs = require('fs');              //檔案讀取
+
+const project = JSON.parse(fs.readFileSync('./service/config/projectInfo.json', 'utf8').toString());
+const version = project.version;
+console.log(version);
 /**----------------------------------------------
  *
  *	 __dirname   絕對路徑，目前檔案所在資料夾路徑。
@@ -18,10 +23,13 @@ console.log(path.sep);
 console.log(path.dirname(__dirname));
 console.log(path.dirname(__dirname)+path.sep+'views');
 console.log('--------------------');
+var res_url = path.normalize(__dirname+path.sep+'..'+path.sep+'..')+path.sep+'web'+path.sep+'dist'+path.sep+version+path.sep;
+console.log(res_url);
+//靜態資源
+app.use(express.static(res_url));
 
 // This is where all the magic happens!
 app.engine('html', swig.renderFile);
-
 app.set('view engine', 'html');
 app.set('views', path.dirname(__dirname)+path.sep+'views'+path.sep+'test');	//設定views所在路徑
 
