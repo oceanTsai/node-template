022f70933800fe812f3bb9a77a5a6c2fc8ec263c
ocean <contest.start@gmai.com>
2016-04-29 17:00:07 +0800

    加入錯誤處理

--- service/appCore/app.js
+++ service/appCore/app.js
@@ -1,25 +1,38 @@
 'use strict';
 (function(module){
-  const express     = require('express');
-  const app         = express();
-  const swig        = require('swig');
-  const path        = require('path');
-  const project     = require('../config/projectInfo.json');
-  const version     = project.version;
-  const port        = project.port;
-  const viewsRouter = require('../controller/viewsRouter.js');
+  //const https       = require('https');
+  const express       = require('express');
+  const app           = express();
+  const swig          = require('swig');
+  const cookieParser  = require('cookie-parser');
+  const path          = require('path');
+  const project       = require('../config/projectInfo.json');
+  const version       = project.version;
+  const port          = project.port;
+  const viewsRouter   = require('../controller/viewsRouter.js');
+  /* use https
+  var fs = require('fs');
+  var options = {
+    key: fs.readFileSync('E:/ssl/myserver.key'),
+    cert: fs.readFileSync('E:/ssl/myserver.crt'),
+    passphrase: '1234'
+  };
+  var server = null;
+  */
+
+  var deployMiddleware = function(){
+    app.use(cookieParser());
+  };
 
   //設定靜態資源路徑
   var deployResource = function(){
     var res_url     = path.resolve(__dirname, '../../web/dist/'+version+'/');
     app.use(express.static(res_url));
-    console.log(res_url);
   };
 
   //
   var deployViewEngine = function(){
     var views_path  = path.resolve(__dirname, '../views');
-      console.log(views_path);
     app.engine('html', swig.renderFile);  
     app.set('view engine', 'html');
     app.set('views', views_path);         //設定views所在路徑
@@ -28,22 +41,33 @@
   };
 
   //
-  var deplayRouterHandler = function(app){
+  var deplayRouterHandler = function(){
     viewsRouter.initialize(app);
   };
 
   //
   var listen = function(){
     app.listen(port);
+    /* https
+    server = https.createServer(options, app);
+    server.listen(port);
+    */
     console.log('Application Started on http://localhost:'+port+'/');
   };
 
   //start app server
   var start = function(){
-    deployResource();
-    deployViewEngine();
-    deplayRouterHandler(app);
-    listen();
+      deployResource();
+      deployViewEngine();
+      deplayRouterHandler();
+      deployMiddleware();
+      app.use(function(err, req, res, next) {
+        console.error(err.stack);
+        res.status(500).send('Something broke!');
+      });
+      listen();
+
+
   };
     
   module.exports = {
--- service/controller/viewsRouter.js
+++ service/controller/viewsRouter.js
@@ -1,13 +1,14 @@
 'use strict';
-(function(module){
-	
-	var initialize = function(app, resource, data){
-		app.get('/', function (req, res) {
-			res.render('test/test', { title : '測試'});
+(function(module){	
+	var initialize = function(app, path, resource, data){
+
+		app.get(/\w+\.html$/, function (req, res) {
+			var viewUrl = req.url.substring(1,req.url.length);
+			res.render(viewUrl, { title : '測試'});
 		});
 	};
 	
 	module.exports = {
 		initialize : initialize
-  };
+	};
 })(module);
