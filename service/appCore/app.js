4942c4196cee0f2982706a5ccb6fa64647578410
ocean <contest.start@gmai.com>
2016-04-29 18:46:20 +0800

    viewRouter 加入404機制 & 例外處理機制.

--- service/appCore/app.js
+++ service/appCore/app.js
@@ -61,10 +61,11 @@
       deployViewEngine();
       deplayRouterHandler();
       deployMiddleware();
+      /*
       app.use(function(err, req, res, next) {
         console.error(err.stack);
         res.status(500).send('Something broke!');
-      });
+      });*/
       listen();
 
 
--- service/controller/viewsRouter.js
+++ service/controller/viewsRouter.js
@@ -4,8 +4,30 @@
 
 		app.get(/\w+\.html$/, function (req, res) {
 			var viewUrl = req.url.substring(1,req.url.length);
-			res.render(viewUrl, { title : '測試'});
+			var fs = require('fs');
+			var path = require('path');
+			var filePath = path.resolve(__dirname, '../views/'+viewUrl);
+			console.log(filePath);
+			fs.exists(filePath, function (exists) {
+				//這裡需要檢查檔案存在與否
+				//console.log(exists);
+				if(exists){
+					res.status(200);
+					res.render(viewUrl, { title : '測試'});	
+				}else{
+					res.status(404);
+					res.render('404.html', { message : 'not fount page', status : '404'});
+				}
+			});
 		});
+
+		app.use(function(err, req, res, next){
+			console.log('----------------');
+			//發生例外
+		  //res.status(500).send({ error: 'Something failed!' });   
+		  //res.sendStatus(500); // 等于 res.status(500).send('500') 
+    });
+		
 	};
 	
 	module.exports = {
